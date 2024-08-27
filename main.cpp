384ff9f385b9b5233ba37a9da0dcc2ba88d872a7
hjc2000 <hjc.666@foxmail.com>
2024-08-27 13:27:11 +0800

    1

--- main.cpp
+++ main.cpp
@@ -4,11 +4,36 @@
 
 using namespace std;
 
+class Test
+{
+public:
+    Test()
+    {
+        std::cout << this << std::endl;
+        throw std::runtime_error{"构造函数中抛出异常"};
+    }
+
+    ~Test()
+    {
+        std::cout << "~Test" << std::endl;
+    }
+};
+
 int main()
 {
     try
     {
-        Test_SpringMode();
+        Test *p = nullptr;
+        try
+        {
+            p = new Test{};
+        }
+        catch (std::exception const &e)
+        {
+            std::cout << e.what() << std::endl;
+        }
+
+        std::cout << p << std::endl;
         return 0;
     }
     catch (std::exception const &e)
